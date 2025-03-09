e47351177a1765a78bfce970158a1dc55cf4cc29
litetex <40789489+litetex@users.noreply.github.com>
2025-03-10 00:33:35 +0100

    Support keeping scroll positions (#837)
    * Save mod list scroll position
    
    * Save scroll position for mod description widget
    
    * Description: Don't check for updates on RENDER Thread
    
    Instead of checking if the selected mod changed during RENDER time (on the rendering thread) better do that only when an actual change occurs. This is way more performant.
    
    + Some additional minor cleanups
    
    - Fix scroll position being reset when returning to the Mods screen
    - Optimize description rendering

--- src/main/java/com/terraformersmc/modmenu/gui/ModsScreen.java
+++ src/main/java/com/terraformersmc/modmenu/gui/ModsScreen.java
@@ -241,6 +241,7 @@ public class ModsScreen extends Screen {
 			this.height - RIGHT_PANE_Y - 96,
 			RIGHT_PANE_Y + 60,
 			textRenderer.fontHeight + 1,
+			this.descriptionListWidget,
 			this
 		);
 		this.descriptionListWidget.setX(this.rightPaneX);
@@ -256,7 +257,7 @@ public class ModsScreen extends Screen {
 		}).position(this.width / 2 + 4, this.height - 28).size(150, 20).build();
 
 		// Initialize data
-		modList.reloadFilters();
+		modList.finalizeInit();
 		this.setFilterOptionsShown(this.keepFilterOptionsShown && this.filterOptionsShown);
 
 		// Add children
@@ -514,40 +515,38 @@ public class ModsScreen extends Screen {
 	}
 
 	public void updateSelectedEntry(ModListEntry entry) {
-		if (entry != null) {
-			this.selected = entry;
-			String modId = selected.getMod().getId();
-
-			if (this.configureButton != null) {
-
-				this.configureButton.active = getModHasConfigScreen(modId);
-				this.configureButton.visible =
-					selected != null && getModHasConfigScreen(modId) || modScreenErrors.containsKey(modId);
-
-				if (modScreenErrors.containsKey(modId)) {
-					Throwable e = modScreenErrors.get(modId);
-					this.configureButton.setTooltip(Tooltip.of(ModMenuScreenTexts.configureError(modId, e)));
-				} else {
-					this.configureButton.setTooltip(Tooltip.of(ModMenuScreenTexts.CONFIGURE));
-				}
-			}
-
-			var isMinecraft = modId.equals("minecraft");
-
-			if (isMinecraft) {
-				this.websiteButton.setMessage(SEND_FEEDBACK_TEXT);
-				this.issuesButton.setMessage(REPORT_BUGS_TEXT);
-			} else {
-				this.websiteButton.setMessage(ModMenuScreenTexts.WEBSITE);
-				this.issuesButton.setMessage(ModMenuScreenTexts.ISSUES);
-			}
-
-			this.websiteButton.visible = true;
-			this.websiteButton.active = isMinecraft || selected.getMod().getWebsite() != null;
-
-			this.issuesButton.visible = true;
-			this.issuesButton.active = isMinecraft || selected.getMod().getIssueTracker() != null;
+		if (entry == null) {
+			return;
 		}
+
+		this.selected = entry;
+		String modId = selected.getMod().getId();
+
+		this.descriptionListWidget.updateSelectedModIfRequired(selected.getMod());
+
+		if (this.configureButton != null) {
+
+			this.configureButton.active = getModHasConfigScreen(modId);
+			this.configureButton.visible =
+				selected != null && getModHasConfigScreen(modId) || modScreenErrors.containsKey(modId);
+
+			if (modScreenErrors.containsKey(modId)) {
+				Throwable e = modScreenErrors.get(modId);
+				this.configureButton.setTooltip(Tooltip.of(ModMenuScreenTexts.configureError(modId, e)));
+			} else {
+				this.configureButton.setTooltip(Tooltip.of(ModMenuScreenTexts.CONFIGURE));
+			}
+		}
+
+		boolean isMinecraft = modId.equals("minecraft");
+		this.websiteButton.setMessage(isMinecraft ? SEND_FEEDBACK_TEXT : ModMenuScreenTexts.WEBSITE);
+		this.issuesButton.setMessage(isMinecraft ? REPORT_BUGS_TEXT : ModMenuScreenTexts.ISSUES);
+
+		this.websiteButton.visible = true;
+		this.websiteButton.active = isMinecraft || selected.getMod().getWebsite() != null;
+
+		this.issuesButton.visible = true;
+		this.issuesButton.active = isMinecraft || selected.getMod().getIssueTracker() != null;
 	}
 
 	public double getScrollPercent() {
--- src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -5,7 +5,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.terraformersmc.modmenu.api.UpdateInfo;
 import com.terraformersmc.modmenu.config.ModMenuConfig;
 import com.terraformersmc.modmenu.gui.ModsScreen;
-import com.terraformersmc.modmenu.gui.widget.entries.ModListEntry;
 import com.terraformersmc.modmenu.util.mod.Mod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
@@ -51,7 +50,7 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 
 	private final ModsScreen parent;
 	private final TextRenderer textRenderer;
-	private ModListEntry lastSelected = null;
+	private Mod selectedMod = null;
 
 	public DescriptionListWidget(
 		MinecraftClient client,
@@ -59,11 +58,21 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 		int height,
 		int y,
 		int itemHeight,
+		DescriptionListWidget copyFrom,
 		ModsScreen parent
 	) {
 		super(client, width, height, y, itemHeight);
 		this.parent = parent;
 		this.textRenderer = client.textRenderer;
+
+		if(copyFrom != null) {
+			updateSelectedModIfRequired(copyFrom.selectedMod);
+			setScrollY(copyFrom.getScrollY());
+		}
+
+		if(parent.getSelectedEntry() != null) {
+			updateSelectedModIfRequired(parent.getSelectedEntry().getMod());
+		}
 	}
 
 	@Override
@@ -83,185 +92,195 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 
 	@Override
 	public void appendClickableNarrations(NarrationMessageBuilder builder) {
-		Mod mod = parent.getSelectedEntry().getMod();
-		builder.put(NarrationPart.TITLE, mod.getTranslatedName() + " " + mod.getPrefixedVersion());
+		if(selectedMod != null) {
+			builder.put(
+				NarrationPart.TITLE,
+				selectedMod.getTranslatedName() + " " + selectedMod.getPrefixedVersion());
+		}
 	}
 
-	@Override
-	public void renderList(DrawContext DrawContext, int mouseX, int mouseY, float delta) {
-		ModListEntry selectedEntry = parent.getSelectedEntry();
-		if (selectedEntry != lastSelected) {
-			lastSelected = selectedEntry;
-			clearEntries();
-			setScrollY(-Double.MAX_VALUE);
-			if (lastSelected != null) {
-				DescriptionEntry emptyEntry = new DescriptionEntry(OrderedText.EMPTY);
-				int wrapWidth = getRowWidth() - 5;
+	private void rebuildUI() {
+		if (selectedMod == null) {
+			return;
+		}
 
-				Mod mod = lastSelected.getMod();
-				Text description = mod.getFormattedDescription();
-				if (!description.getString().isEmpty()) {
-					for (OrderedText line : textRenderer.wrapLines(description, wrapWidth)) {
-						children().add(new DescriptionEntry(line));
+		DescriptionEntry emptyEntry = new DescriptionEntry(OrderedText.EMPTY);
+		int wrapWidth = getRowWidth() - 5;
+
+		Mod mod = selectedMod;
+		Text description = mod.getFormattedDescription();
+		if (!description.getString().isEmpty()) {
+			for (OrderedText line : textRenderer.wrapLines(description, wrapWidth)) {
+				children().add(new DescriptionEntry(line));
+			}
+		}
+
+		if (ModMenuConfig.UPDATE_CHECKER.getValue() && !ModMenuConfig.DISABLE_UPDATE_CHECKER.getValue()
+			.contains(mod.getId())) {
+			UpdateInfo updateInfo = mod.getUpdateInfo();
+			if (updateInfo != null && updateInfo.isUpdateAvailable()) {
+				children().add(emptyEntry);
+
+				int index = 0;
+				for (OrderedText line : textRenderer.wrapLines(HAS_UPDATE_TEXT, wrapWidth - 11)) {
+					DescriptionEntry entry = new DescriptionEntry(line);
+					if (index == 0) {
+						entry.setUpdateTextEntry();
 					}
+
+					children().add(entry);
+					index += 1;
 				}
 
-				if (ModMenuConfig.UPDATE_CHECKER.getValue() && !ModMenuConfig.DISABLE_UPDATE_CHECKER.getValue()
-					.contains(mod.getId())) {
-					UpdateInfo updateInfo = mod.getUpdateInfo();
-					if (updateInfo != null && updateInfo.isUpdateAvailable()) {
-						children().add(emptyEntry);
-
-						int index = 0;
-						for (OrderedText line : textRenderer.wrapLines(HAS_UPDATE_TEXT, wrapWidth - 11)) {
-							DescriptionEntry entry = new DescriptionEntry(line);
-							if (index == 0) {
-								entry.setUpdateTextEntry();
-							}
-
-							children().add(entry);
-							index += 1;
-						}
-
-						for (OrderedText line : textRenderer.wrapLines(EXPERIMENTAL_TEXT, wrapWidth - 16)) {
-							children().add(new DescriptionEntry(line, 8));
-						}
-
-
-						Text updateMessage = updateInfo.getUpdateMessage();
-						String downloadLink = updateInfo.getDownloadLink();
-						if (updateMessage == null) {
-							updateMessage = DOWNLOAD_TEXT;
-						} else {
-							if (downloadLink != null) {
-								updateMessage = updateMessage.copy()
-									.formatted(Formatting.BLUE)
-									.formatted(Formatting.UNDERLINE);
-							}
-						}
-						for (OrderedText line : textRenderer.wrapLines(updateMessage, wrapWidth - 16)) {
-							if (downloadLink != null) {
-								children().add(new LinkEntry(line, downloadLink, 8));
-							} else {
-								children().add(new DescriptionEntry(line, 8));
-
-							}
-						}
-					}
-					if (mod.getChildHasUpdate()) {
-						children().add(emptyEntry);
-
-						int index = 0;
-						for (OrderedText line : textRenderer.wrapLines(CHILD_HAS_UPDATE_TEXT, wrapWidth - 11)) {
-							DescriptionEntry entry = new DescriptionEntry(line);
-							if (index == 0) {
-								entry.setUpdateTextEntry();
-							}
-
-							children().add(entry);
-							index += 1;
-						}
-					}
+				for (OrderedText line : textRenderer.wrapLines(EXPERIMENTAL_TEXT, wrapWidth - 16)) {
+					children().add(new DescriptionEntry(line, 8));
 				}
 
-				Map<String, String> links = mod.getLinks();
-				String sourceLink = mod.getSource();
-				if ((!links.isEmpty() || sourceLink != null) && !ModMenuConfig.HIDE_MOD_LINKS.getValue()) {
+
+				Text updateMessage = updateInfo.getUpdateMessage();
+				String downloadLink = updateInfo.getDownloadLink();
+				if (updateMessage == null) {
+					updateMessage = DOWNLOAD_TEXT;
+				} else {
+					if (downloadLink != null) {
+						updateMessage = updateMessage.copy()
+							.formatted(Formatting.BLUE)
+							.formatted(Formatting.UNDERLINE);
+					}
+				}
+				for (OrderedText line : textRenderer.wrapLines(updateMessage, wrapWidth - 16)) {
+					if (downloadLink != null) {
+						children().add(new LinkEntry(line, downloadLink, 8));
+					} else {
+						children().add(new DescriptionEntry(line, 8));
+
+					}
+				}
+			}
+			if (mod.getChildHasUpdate()) {
+				children().add(emptyEntry);
+
+				int index = 0;
+				for (OrderedText line : textRenderer.wrapLines(CHILD_HAS_UPDATE_TEXT, wrapWidth - 11)) {
+					DescriptionEntry entry = new DescriptionEntry(line);
+					if (index == 0) {
+						entry.setUpdateTextEntry();
+					}
+
+					children().add(entry);
+					index += 1;
+				}
+			}
+		}
+
+		Map<String, String> links = mod.getLinks();
+		String sourceLink = mod.getSource();
+		if ((!links.isEmpty() || sourceLink != null) && !ModMenuConfig.HIDE_MOD_LINKS.getValue()) {
+			children().add(emptyEntry);
+
+			for (OrderedText line : textRenderer.wrapLines(LINKS_TEXT, wrapWidth)) {
+				children().add(new DescriptionEntry(line));
+			}
+
+			if (sourceLink != null) {
+				int indent = 8;
+				for (OrderedText line : textRenderer.wrapLines(SOURCE_TEXT, wrapWidth - 16)) {
+					children().add(new LinkEntry(line, sourceLink, indent));
+					indent = 16;
+				}
+			}
+
+			links.forEach((key, value) -> {
+				int indent = 8;
+				for (OrderedText line : textRenderer.wrapLines(Text.translatable(key)
+						.formatted(Formatting.BLUE)
+						.formatted(Formatting.UNDERLINE),
+					wrapWidth - 16
+				)) {
+					children().add(new LinkEntry(line, value, indent));
+					indent = 16;
+				}
+			});
+		}
+
+		Set<String> licenses = mod.getLicense();
+		if (!ModMenuConfig.HIDE_MOD_LICENSE.getValue() && !licenses.isEmpty()) {
+			children().add(emptyEntry);
+
+			for (OrderedText line : textRenderer.wrapLines(LICENSE_TEXT, wrapWidth)) {
+				children().add(new DescriptionEntry(line));
+			}
+
+			for (String license : licenses) {
+				int indent = 8;
+				for (OrderedText line : textRenderer.wrapLines(Text.literal(license), wrapWidth - 16)) {
+					children().add(new DescriptionEntry(line, indent));
+					indent = 16;
+				}
+			}
+		}
+
+		if (!ModMenuConfig.HIDE_MOD_CREDITS.getValue()) {
+			if ("minecraft".equals(mod.getId())) {
+				children().add(emptyEntry);
+
+				for (OrderedText line : textRenderer.wrapLines(VIEW_CREDITS_TEXT, wrapWidth)) {
+					children().add(new MojangCreditsEntry(line));
+				}
+			} else if (!"java".equals(mod.getId())) {
+				var credits = mod.getCredits();
+
+				if (!credits.isEmpty()) {
 					children().add(emptyEntry);
 
-					for (OrderedText line : textRenderer.wrapLines(LINKS_TEXT, wrapWidth)) {
+					for (OrderedText line : textRenderer.wrapLines(CREDITS_TEXT, wrapWidth)) {
 						children().add(new DescriptionEntry(line));
 					}
 
-					if (sourceLink != null) {
-						int indent = 8;
-						for (OrderedText line : textRenderer.wrapLines(SOURCE_TEXT, wrapWidth - 16)) {
-							children().add(new LinkEntry(line, sourceLink, indent));
-							indent = 16;
-						}
-					}
+					var iterator = credits.entrySet().iterator();
 
-					links.forEach((key, value) -> {
+					while (iterator.hasNext()) {
 						int indent = 8;
-						for (OrderedText line : textRenderer.wrapLines(Text.translatable(key)
-								.formatted(Formatting.BLUE)
-								.formatted(Formatting.UNDERLINE),
+
+						var role = iterator.next();
+						var roleName = role.getKey();
+
+						for (var line : textRenderer.wrapLines(this.creditsRoleText(roleName),
 							wrapWidth - 16
 						)) {
-							children().add(new LinkEntry(line, value, indent));
-							indent = 16;
-						}
-					});
-				}
-
-				Set<String> licenses = mod.getLicense();
-				if (!ModMenuConfig.HIDE_MOD_LICENSE.getValue() && !licenses.isEmpty()) {
-					children().add(emptyEntry);
-
-					for (OrderedText line : textRenderer.wrapLines(LICENSE_TEXT, wrapWidth)) {
-						children().add(new DescriptionEntry(line));
-					}
-
-					for (String license : licenses) {
-						int indent = 8;
-						for (OrderedText line : textRenderer.wrapLines(Text.literal(license), wrapWidth - 16)) {
 							children().add(new DescriptionEntry(line, indent));
 							indent = 16;
 						}
-					}
-				}
 
-				if (!ModMenuConfig.HIDE_MOD_CREDITS.getValue()) {
-					if ("minecraft".equals(mod.getId())) {
-						children().add(emptyEntry);
+						for (var contributor : role.getValue()) {
+							indent = 16;
 
-						for (OrderedText line : textRenderer.wrapLines(VIEW_CREDITS_TEXT, wrapWidth)) {
-							children().add(new MojangCreditsEntry(line));
+							for (var line : textRenderer.wrapLines(Text.literal(contributor), wrapWidth - 24)) {
+								children().add(new DescriptionEntry(line, indent));
+								indent = 24;
+							}
 						}
-					} else if (!"java".equals(mod.getId())) {
-						var credits = mod.getCredits();
 
-						if (!credits.isEmpty()) {
+						if (iterator.hasNext()) {
 							children().add(emptyEntry);
-
-							for (OrderedText line : textRenderer.wrapLines(CREDITS_TEXT, wrapWidth)) {
-								children().add(new DescriptionEntry(line));
-							}
-
-							var iterator = credits.entrySet().iterator();
-
-							while (iterator.hasNext()) {
-								int indent = 8;
-
-								var role = iterator.next();
-								var roleName = role.getKey();
-
-								for (var line : textRenderer.wrapLines(this.creditsRoleText(roleName),
-									wrapWidth - 16
-								)) {
-									children().add(new DescriptionEntry(line, indent));
-									indent = 16;
-								}
-
-								for (var contributor : role.getValue()) {
-									indent = 16;
-
-									for (var line : textRenderer.wrapLines(Text.literal(contributor), wrapWidth - 24)) {
-										children().add(new DescriptionEntry(line, indent));
-										indent = 24;
-									}
-								}
-
-								if (iterator.hasNext()) {
-									children().add(emptyEntry);
-								}
-							}
 						}
 					}
 				}
 			}
 		}
+	}
 
+	public void updateSelectedModIfRequired(Mod mod) {
+		if (mod != selectedMod) {
+			selectedMod = mod;
+			clearEntries();
+			setScrollY(-Double.MAX_VALUE);
+			rebuildUI();
+		}
+	}
+
+	@Override
+	public void renderList(DrawContext DrawContext, int mouseX, int mouseY, float delta) {
 		Tessellator tessellator = Tessellator.getInstance();
 		BufferBuilder bufferBuilder;
 		BuiltBuffer builtBuffer;
